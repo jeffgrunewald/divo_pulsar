b49d7aefe4b982b1ca28b54bfc5c73cebd1ee817
jeffgrunewald <jeff@grunewalddesign.com>
2021-01-13 10:59:13 -0500

    version bump for release

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule DivoPulsar.MixProject do
   def project() do
     [
       app: :divo_pulsar,
-      version: "0.1.1",
+      version: "0.1.2",
       elixir: "~> 1.8",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
